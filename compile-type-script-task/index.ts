74f017a510507f6dfdbcf76c724fe953437bff4d
Martin Nilsson <martin.h.nilsson@gmail.com>
2017-03-13 14:44:47 +0100

    adds tsc out dir

--- compile-type-script-task/index.ts
+++ compile-type-script-task/index.ts
@@ -68,7 +68,7 @@ async function run() {
                     compileProject(tsc, projectPath);
                 }
                 else{
-                    compileFiles(tsc, files);
+                    compileFiles(tsc, filesPath, files);
                 }
                 console.log('Compilation completed');
             }
@@ -113,11 +113,11 @@ function compileProject(tsc: string, path: string){
     }
 }
 
-function compileFiles(tsc: string, files: string[]){
+function compileFiles(tsc: string, filesPath: string, files: string[]){
     task.debug('function=compileFiles');
 
     let compiler = task.tool(task.which('node', true));
-    compiler.arg(tsc);
+    compiler.arg(tsc).arg('--outDir').arg(filesPath);
     files.forEach(file => {
         compiler.arg(file);
     });
